ab1835b635672350431e7a488715e78a9be0d364
brocode <kelimbth@gmail.com>
2023-03-02 12:43:28 +0300

    updated project configurations

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,3 +116,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+    messages.error: 'danger',
+}
