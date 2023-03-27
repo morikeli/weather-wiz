240a94801654e79f125ecc40917864b13c1f25bb
brocode <kelimbth@gmail.com>
2023-03-27 22:58:33 +0300

    updated configs.

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -111,7 +112,7 @@ STATIC_URL = 'static/'
 #     BASE_DIR, 'static',
 # ]
 
-STATIC_ROOT = BASE_DIR/'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
