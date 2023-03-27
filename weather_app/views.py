0242bc10f7ed62fc10f41dd2da240cb334f83983
brocode <kelimbth@gmail.com>
2023-03-27 21:33:41 +0300

    minor changes

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,35 +1,35 @@
-from django.shortcuts import render
-from django.contrib import messages
+from django.shortcuts import render, redirect
 import requests
-import json
+
 
 def index_view(request):
     context = {}
-    if request.method == 'POST':
-        API_KEY = '5832d3fb7821368ed47f17f2b4cbfb18'
+    if request.method == 'GET':
+        API_KEY = ''
         BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
 
-        city = request.POST['search-form']
+        city = request.GET.get('city')
 
         request_url = f'{BASE_URL}?appid={API_KEY}&q={city}&units=metric'
         response = requests.get(request_url)
 
         if response.status_code == 200:
             data = response.json()
-            print(data)
+            
             longitude = data['coord']['lon']
             latitude = data['coord']['lat']
             country = data['sys']['country']
+            condition = data['weather'][0]['main']
             weather = data['weather'][0]['description']
             temp = data['main']['temp']
             wind_speed = data['wind']['speed']
             humidity = data['main']['humidity']
-            pressure = data['main']['pressure']
             icon = data['weather'][0]['icon']
 
-            context = {'city':str(city).title(), 'weather': str(weather).title(), 'temperature': temp, 'country': country,
-             'long': round(longitude, 2), 'lat': round(latitude, 2), 'humidity':humidity, 'wind_speed': wind_speed, 'icon': icon}
+            context = {'city':city, 'weather': str(weather).capitalize(), 'temperature': temp, 'country': country,
+                'long': round(longitude, 2), 'lat': round(latitude, 2), 'humidity': humidity, 'wind_speed': wind_speed, 'icon': icon,
+                'weather_condition': condition,
+            }
         
-
     return render(request, 'weather-app/index.html', context)
 
